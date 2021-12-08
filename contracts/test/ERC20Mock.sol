161d198ef22b759ea74782d7f2f61d7614897a3f
gfijrb <gfijrb@yandex.ua>
2021-12-08 20:43:25 +0200

    add test contracts

---
+++ contracts/test/ERC20Mock.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.2;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract ERC20Mock is ERC20, Ownable {
+    constructor(
+        string memory name,
+        string memory symbol
+    ) ERC20(name, symbol) {}
+
+    function mint(address account, uint256 amount) external onlyOwner {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) external onlyOwner {
+        _burn(account, amount);
+    }
+}
